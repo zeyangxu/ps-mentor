c6a867c514660090eac57b4f78da39027ba5e9fe
Zeyang Xu <43708262+zeyangxu@users.noreply.github.com>
2024-12-23 19:58:13 +1100

    121

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -14,20 +14,20 @@ const Login = () => {
 
         <div className="max-w-xl w-full mx-auto px-6 relative animate-fade-up">
           <div className="flex justify-center mb-8">
-            <img 
-              src="/lovable-uploads/392db423-89a7-4be0-be5f-d508e73d5651.png" 
-              alt="PS Logo" 
+            <img
+              src="/lovable-uploads/392db423-89a7-4be0-be5f-d508e73d5651.png"
+              alt="PS Logo"
               className="w-20 h-20 hover:scale-110 transition-transform duration-300"
             />
           </div>
           <Alert className="mb-6">
             <InfoIcon className="h-4 w-4" />
             <AlertDescription className="ml-2">
-              目前我们仅支持邮箱这一种注册方式，给您带来不便，还请谅解。
+              目前Dr. PS Checker 仅支持邮箱这一种注册方式，给您带来不便，还请谅解。
               <br />
-              点击注册后，请前往您的邮箱点击确认链接完成注册。
+              点击注册后，请前往您的邮箱，点击Supabase发来邮件中的确认链接完成注册。
               <br />
-              注册成功后，您可以免费体验2次反馈超千字的文书深度评估。😊
+              注册成功后，您可以免费体验1次反馈超千字的文书深度评估。😊
             </AlertDescription>
           </Alert>
           <div className="w-full bg-card rounded-lg shadow-lg p-8 relative z-10 hover:scale-[1.01] transition-transform duration-300">

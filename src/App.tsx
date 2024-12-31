e6197222ef23de6ad6716f2805e9bd15a5f30a44
Zeyang Xu <43708262+zeyangxu@users.noreply.github.com>
2024-12-31 08:46:29 +0000

    add analytics

--- src/App.tsx
+++ src/App.tsx
@@ -2,31 +2,35 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { MinimalNav } from "@/components/MinimalNav";
 import Index from "./pages/Index";
 import Editor from "./pages/Editor";
 import Login from "./pages/Login";
 import Payment from "./pages/Payment";
+import { Analytics } from "@vercel/analytics/react";
 
 const queryClient = new QueryClient();
 
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <MinimalNav />
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/editor" element={<Editor />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/payment" element={<Payment />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
+  <div>
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <MinimalNav />
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/editor" element={<Editor />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/payment" element={<Payment />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
+    <Analytics />
+  </div>
 );
 
 export default App;
